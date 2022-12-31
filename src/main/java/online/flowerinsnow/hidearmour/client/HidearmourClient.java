20fccd404dc664f3b79ba5a2622e8813c935c513
冬花ice <flowerinsnow@donghua.email.cn>
2022-12-31 03:34:16 +0000

    1.19.3 fabric

---
+++ src/main/java/online/flowerinsnow/hidearmour/client/HidearmourClient.java
@@ -0,0 +1,13 @@
+package online.flowerinsnow.hidearmour.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class HidearmourClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/online/flowerinsnow/hidearmour/client/command/CommandHideArmour.java
@@ -0,0 +1,4 @@
+package online.flowerinsnow.hidearmour.client.command;
+
+public class CommandHideArmour {
+}
