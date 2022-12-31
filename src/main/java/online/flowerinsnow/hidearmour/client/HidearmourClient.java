f319ee9b9ff04ffd8b904ed314c31a4fcc3f4284
冬花ice <flowerinsnow@donghua.email.cn>
2022-12-31 05:44:19 +0000

    什么我居然没有加载配置文件

--- src/main/java/online/flowerinsnow/hidearmour/client/HidearmourClient.java
+++ src/main/java/online/flowerinsnow/hidearmour/client/HidearmourClient.java
@@ -4,13 +4,22 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import online.flowerinsnow.hidearmour.client.command.CommandHideArmour;
+import online.flowerinsnow.hidearmour.client.config.Config;
 import online.flowerinsnow.hidearmour.client.eci.RenderPlayerEntityCallback;
 import online.flowerinsnow.hidearmour.client.listener.RenderListener;
+
+import java.io.IOException;
 
 @Environment(EnvType.CLIENT)
 public class HidearmourClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        try {
+            Config.loadConfig();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         CommandHideArmour.register();
 
         RenderListener renderListener = new RenderListener();
